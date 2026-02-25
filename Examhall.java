e60f80bfd47cf8def417bb9abb017632c0d50568
sifaulzabeen6-art <sifaulzabeen6@gmail.com>
2026-02-25 14:33:56 +0530

    Add files via upload
    my program

---
+++ Examhall.java
@@ -0,0 +1,7 @@
+class Examhall
+{
+	public static void main(String[]args)
+	{
+		System.out.println("No talking,only writing!");
+	}
+}
---
+++ coffeeshop.java
@@ -0,0 +1,7 @@
+class coffeeshop
+{
+	public static void main(String []args)
+	{
+		System.out.println("one strong filter coffee");
+	}
+}
